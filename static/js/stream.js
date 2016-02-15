dfd50c8fc78b58486437926587fe78b0bcc7e711
Vaga <fabien.casters@gmail.com>
2016-02-15 17:19:52 +0100

    Added button all for the application filters and color on the deployment

--- static/js/stream.js
+++ static/js/stream.js
@@ -25,7 +25,12 @@
         webSocket.onmessage = function (event) {
             console.debug('[Stream] OnMessage');
             console.debug('[Stream] ', event.data);
-            element.innerHTML = element.innerHTML + event.data;
+            if (event.data.substr(0,2) == 'ok')
+                element.innerHTML = element.innerHTML + '<div class="stream-success">' + event.data + '</div>';
+            else if (event.data.substr(0, 5) == 'fatal')
+                element.innerHTML = element.innerHTML + '<div class="stream-error">' + event.data + '</div>';
+            else
+                element.innerHTML = element.innerHTML + '<div>' + event.data + '</div>';
         };
         webSocket.onclose = function () {
             console.debug('[Stream] OnClose');
